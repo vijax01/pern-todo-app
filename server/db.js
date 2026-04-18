5993421655d9cae8183040b11c74c7ed637bd402
Prakansh <vijax01@gmail.com>
2026-04-18 13:35:00 +0530

    Update database connection configuration

--- server/db.js
+++ server/db.js
@@ -1,11 +1,17 @@
-import {Pool} from 'pg';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
+
+import { Pool } from 'pg';
+
+
 const pool = new Pool({
-     connectionString: process.env.DATABASE_URL,
-     ssl: process.env.NODE_ENV === 'production' ? {rejectUnauthorized: false } : false 
-});
+     user: 'neondb_owner',
+     password: 'npg_i6RAolr0VSxE',
+     host: 'ep-floral-silence-amcfclk3-pooler.c-5.us-east-1.aws.neon.tech',
+     database: 'neondb',
+     ssl: { rejectUnauthorized: false }
+   });
+
 
 export default pool;
