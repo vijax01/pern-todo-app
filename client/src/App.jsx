72c8342ab96fb265e116980c14a66f81633013ad
Prakansh <vijax01@gmail.com>
2026-04-18 13:32:59 +0530

    Implement loading state and skeleton UI for messages
    Added loading state management and skeleton loading UI.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { API_URL } from "./api.js"
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css'
 
 function App() {
 
 
-
+     const [loading, setLoading] = useState(false);
      const [inputText, setText] = useState('');
      const [messages, setMessages] = useState([]);
      const [showEditModal, setShowEditModal] = useState(false);
@@ -15,17 +17,17 @@ function App() {
 
      // add data in the database
      const handleSubmit = async () => {
-
           try {
-               await axios.post(`${API_URL}/save`, {
+               const res = await axios.post(`${API_URL}/save`, {
                     text: inputText
-               })
-          } catch (err) {
-               console.log("Here is the valuable error . ", err)
-          }
+               });
+               console.log(res.data);
 
-          setText('');
-          fetchMessages(); // refresh list after saving
+               setText('');
+               await fetchMessages(); // ✅ clean
+          } catch (err) {
+               console.log(err);
+          }
      };
 
 
@@ -34,8 +36,9 @@ function App() {
      const handleDelete = async (id) => {
           if (confirm("Are you sure you want to delete this ?")) {
                try {
-                    await axios.delete(`${API_URL}/delete/${id}`);
-                    fetchMessages(); // refresh after delete
+                    const res = await axios.delete(`${API_URL}/delete/${id}`);
+                    alert(res.data.message); // show backend message "Deleted Successfully"
+                    await fetchMessages(); // refresh after delete
                } catch (err) {
                     console.log("Delete error . ", err)
                }
@@ -57,26 +60,30 @@ function App() {
      const handleUpdateNote = async () => {
           await axios.put(`${API_URL}/update/${editWalaContent.id}`, { text: editWalaContent.text });
           setShowEditModal(false);
-          fetchMessages();
+          await fetchMessages();
      };
 
 
 
      // toggle checkbox in the database
      const toggleCompleted = async (id, checkboxValue) => {
-          await axios.put(`${API_URL}/toggleCheckbox/${id}`, {completed: checkboxValue});
-          fetchMessages();
+          await axios.put(`${API_URL}/toggleCheckbox/${id}`, { completed: checkboxValue });
+          await fetchMessages();
      };
 
 
 
      // fetch messages from the database
      const fetchMessages = async () => {
+          setLoading(true); // ✅ move here
+          console.clear();
           try {
                const res = await axios.get(`${API_URL}/messages`);
                setMessages(res.data);
           } catch (err) {
                console.log("Fetch error:", err);
+          } finally {
+               setLoading(false);
           }
      };
 
@@ -91,6 +98,7 @@ function App() {
 
      // this function runs once on the page load
      useEffect(() => {
+          setLoading(true);
           fetchMessages();
      }, []);
 
@@ -122,32 +130,38 @@ function App() {
                     </div>
                     <div className='p-4' >
                          <h3 className='text-2xl' >Todo Tasks :</h3>
-
-                         {messages.length === 0 ? (
-                              <p className="text-gray-500">
-                                   You don't have any task, let's add one.
-                              </p>
-                         ) : (messages.map((msg, index) => (
-                              <div className='flex items-center text-xl' key={msg.id}>
-                                   <p onClick={() => toggleCompleted(msg.id, msg.completed)} className='mr-2 cursor-pointer flex' >
-                                        {msg.completed ? (
-                                             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="#4ade80" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-check-icon lucide-circle-check"><circle cx="12" cy="12" r="10" /><path d="m9 12 2 2 4-4" /></svg>
-                                        ) : (
-                                             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-icon lucide-circle"><circle cx="12" cy="12" r="10" /></svg>
-                                        )}
+                         {
+                              loading ? (
+                                   <div>
+                                        <Skeleton width="40%" count={10} />
+                                   </div>
+                              ) : (messages.length === 0 ? (
+                                   <p className="text-gray-500">
+                                        You don't have any task, let's add one.
                                    </p>
-                                   <p className='mr-1 cursor-pointer' onClick={() => handleEdit(msg.id, msg.text)} >
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil-icon lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" /><path d="m15 5 4 4" /></svg>                              </p>
-                                   <p className='mr-1 cursor-pointer' onClick={() => handleDelete(msg.id)} >
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6" /><path d="M14 11v6" /><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" /><path d="M3 6h18" /><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /></svg>
-                                   </p>
-                                   <p className={`${msg.completed ? 'line-through' : ''}`} >
-                                        {(index + 1) + ". " + msg.text}
-                                   </p>
-                              </div>
-                         ))
-                         )}
-
+                              ) : (messages.map((msg, index) => (
+                                   <div className='flex items-center text-xl' key={msg.id}>
+                                        <p onClick={() => toggleCompleted(msg.id, msg.completed)} className='mr-2 cursor-pointer flex' >
+                                             {msg.completed ? (
+                                                  <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="#4ade80" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-check-icon lucide-circle-check"><circle cx="12" cy="12" r="10" /><path d="m9 12 2 2 4-4" /></svg>
+                                             ) : (
+                                                  <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-icon lucide-circle"><circle cx="12" cy="12" r="10" /></svg>
+                                             )}
+                                        </p>
+                                        <p className='mr-1 cursor-pointer' onClick={() => handleEdit(msg.id, msg.text)} >
+                                             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil-icon lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" /><path d="m15 5 4 4" /></svg>
+                                        </p>
+                                        <p className='mr-1 cursor-pointer' onClick={() => handleDelete(msg.id)} >
+                                             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6" /><path d="M14 11v6" /><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" /><path d="M3 6h18" /><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /></svg>
+                                        </p>
+                                        <p className={`${msg.completed ? 'line-through' : ''}`} >
+                                             {(index + 1) + ". " + msg.text}
+                                        </p>
+                                   </div>
+                              ))
+                              )
+                              )
+                         }
                     </div>
                </div>
           </>
